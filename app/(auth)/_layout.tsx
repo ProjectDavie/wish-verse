8029142682b79fe9274ff42fac1024c94404b9b8
David Njoroge <davidnjorosalme606@gmail.com>
2026-01-07 11:33:38 +0300

    Auth-context

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,5 +1,14 @@
-import { Slot } from "expo-router";
+import { useAuth } from "@/context/AuthContext";
+import { Redirect, Stack } from "expo-router";
 
 export default function AuthLayout() {
-  return <Slot />;
+  const { isAuthenticated } = useAuth();
+
+  // ✅ If logged in, LEAVE auth screens
+  if (isAuthenticated) {
+    return <Redirect href="/(authenticated)/(tabs)/home" />;
+  }
+
+  // ❌ If not logged in, stay in auth flow
+  return <Stack />;
 }
